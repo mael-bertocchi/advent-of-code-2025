5b7e425811c4f94d3fa3feb5f2f2dd8a3cf75b4a
Maël Bertocchi <ma3l.bth@gmail.com>
2025-12-02 08:03:56 +0100

    feat (day-01): add first and second step solution

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ My solutions for the [Advent of Code 2025 challenges](https://adventofcode.com/2
 
 ## 📅 Progression
 
-| Day | Challenge | Solutions |Stars |
-|-----|-----------|-----------|------|
-|  1  | [Secret Entrance](https://adventofcode.com/2025/day/1) | [View](day-01/src/main.rs) | ⭐⭐ |
+| Day | Challenge | Summary | Solutions | Stars |
+|-----|-----------|---------|-----------|-------|
+|  1  | [Secret Entrance](https://adventofcode.com/2025/day/1) | Simulate a circular safe dial to find a hidden password based on a list of rotation instructions | [View](day-01/src/main.rs) | ⭐⭐ |
+|  2  | [Gift Shop](https://adventofcode.com/2025/day/2) | Calculate the sum of invalid ID numbers found within a list of numerical ranges | [View](day-02/src/main.rs) | ⭐⭐ |
 
 > The stars indicate how many parts of the challenge have been completed. They are two per day.
---
+++ day-02/src/main.rs
@@ -0,0 +1,69 @@
+fn read_file_from_args() -> String {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <input-file>", args[0]);
+        std::process::exit(1);
+    }
+    std::fs::read_to_string(&args[1]).expect("Could not read file")
+}
+
+fn is_repeated_twice(s: &str) -> bool {
+    let bytes = s.as_bytes();
+    let len = bytes.len();
+
+    if len % 2 != 0 {
+        return false;
+    }
+
+    let mid = len / 2;
+    &bytes[..mid] == &bytes[mid..]
+}
+
+fn is_repeated_multiple_times(s: &str) -> bool {
+    let bytes = s.as_bytes();
+    let len = bytes.len();
+
+    for size in 1..=len / 2 {
+        if len % size != 0 {
+            continue;
+        }
+
+        let pattern = &bytes[..size];
+
+        if bytes.chunks(size).all(|chunk| chunk == pattern) {
+            return true;
+        }
+    }
+    false
+}
+
+fn main() {
+    let content = read_file_from_args();
+
+    let mut sequence_repeated_multiple_times: u64 = 0;
+    let mut sequence_repeated_twice: u64 = 0;
+
+    for item in content.trim().split(',') {
+        if let Some((start_str, end_str)) = item.split_once('-') {
+            let start: u64 = start_str.parse().expect("Invalid start");
+            let end: u64 = end_str.parse().expect("Invalid end");
+
+            for num in start..=end {
+                let s: String = num.to_string();
+
+                if is_repeated_twice(&s) {
+                    sequence_repeated_twice += num;
+                }
+
+                if is_repeated_multiple_times(&s) {
+                    sequence_repeated_multiple_times += num;
+                }
+            }
+        }
+    }
+
+    println!(
+        "Sum of sequences repeated twice: {} and sum of sequences repeated at least twice: {}",
+        sequence_repeated_twice, sequence_repeated_multiple_times
+    );
+}
